0d2692619979b154beab23e0f6dd05bd12e4cf7c
Lauren Emick <laurenemick6@gmail.com>
2020-05-22 09:49:43 -0700

    prototypes.js complete

--- challenges/closure.js
+++ challenges/closure.js
@@ -16,9 +16,17 @@ function myFunction() {
 }
 myFunction();
 
-// Explanation: 
-
+// Explanation: nestedFunction() is a closure and has access to variables from a higher level scope (ie parent function myFunction()).
 
 /* Task 2: Counter */
 
-/* Create a function called `sumation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+/* Create a function called `sumation` that accepts a parameter and uses a counter to return the summation of that number. For example, 
+`summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+function sumation(parameter){
+  var counter = 0;
+  for(let i = 0; i < parameter; i ++){
+    
+  } 
+}
+sumation(4);
